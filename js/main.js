8c79f3c310581b1657420602a23301453a6b1981
Hirdmann Konvwolf <sychev.v@gmail.com>
2019-07-02 10:16:12 +0300

    Все готово
    Доделал обработчик событий кнопки "удалить из корзины"
    
    Доделал метод, удаляющий товар из корзины

--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
-const image = "https://placeimg.com/200/150/any";
-const cartImage = "https://placeimg.com/100/80/any";
-
+const image = "https://placeimg.com/200/150/tech";
+const cartImage = "https://placeimg.com/100/80/tech";
 const items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Router', 'USB-camera', 'Gamepad'];
 const prices = [1000, 200, 20, 10, 25, 30, 18, 24];
 const ids = [1, 2, 3, 4, 5, 6, 7, 8];
 const userCart = [];
-
-function fetchData() {
-	let arr = [];
-	for (let i = 0; i < items.length; i++) {
-		arr.push({
-			title: items[i],
-			price: prices[i],
-			img: image,
-			id: ids[i]
-		});
-	}
-	return arr
-}
 
 class ProductList {
 	constructor() {
@@ -29,7 +15,14 @@ class ProductList {
 		this.render()
 	}
 	fetchProducts() {
-		this.products = fetchData()
+		for (let i = 0; i < items.length; i++) {
+			this.products.push({
+				title: items[i],
+				price: prices[i],
+				img: image,
+				id: ids[i]
+			});
+		}
 	}
 	render() {
 		const block = document.querySelector('.products')
@@ -63,6 +56,15 @@ class Product {
 	}
 }
 
+/**
+ * Класс, вешающий обработчики событий на кнопки "Купить", "Корзина" и "х". Обработчики
+ * на кнопках "Купить" и "х" вызывают класс @CartActions
+ * @listenerForCart - обработчик событий на кнопку "Купить"
+ * @listenerForCartBody - обработчик событий на кнопку "Корзина"
+ * @listenerForDelButton - обработчик на кнопку "х" (удалить товар из корзины)
+ *
+ * @class Listeners
+ */
 class Listeners {
 	constructor() {
 		this.allProducts = document.querySelector('.products')
@@ -72,15 +74,15 @@ class Listeners {
 	}
 
 	_init() {
-		this.listenerForCart();
-		this.listenerForCartBody();
+		this.listenerForCart()
+		this.listenerForCartBody()
 		this.listenerForDelButton()
 	}
 
 	listenerForCart() {
 		this.allProducts.addEventListener('click', (evt) => {
 			if (evt.target.classList.contains('buy-btn')) {
-				let add = new CartProducts(evt.target)
+				let add = new CartActions(evt.target)
 			}
 		})
 	}
@@ -91,11 +93,10 @@ class Listeners {
 		})
 	}
 
-	TODO:listenerForDelButton() {
+	listenerForDelButton() {
 		this.cartBody.addEventListener('click', (evt) => {
 			if (evt.target.classList.contains('del-btn')) {
-				let remove = new CartProducts(evt.target)
-				console.log(evt.target)
+				let remove = new CartActions(evt.target)
 			}
 		})
 	}
@@ -103,14 +104,32 @@ class Listeners {
 
 //CART
 
-class CartProducts {
+/**
+ * Класс действий над корзиной: добавление товаров, подсчет количества, вывод нарастающего
+ * итога. Переменная @buy используется для проверки, нажата ли кнопка "Купить", переменная
+ * @delete - нажата ли кнопка "х" (удалить товар из корзины). Если buy возвращает true, то
+ * вызывается метод @addProductToCart (добавляет товар в корзину и считает количество). Если
+ * delete возвращает true, то вызывается метод @deleteProductFromCart (удалят товар из корзины
+ * и пересчитывает количество). Метод @render вызывает класс @Cart
+ *
+ * @class CartActions
+ */
+class CartActions {
 	constructor(event) {
 		this.find = userCart.find(element => element.id === event.dataset['id'])
+		this.buy = event.classList.contains('buy-btn')
+		this.delete = event.classList.contains('del-btn')
 		this._init(event)
 	}
 
 	_init(event) {
-		this.addProductToCart(event)
+		if (this.buy) {
+			this.addProductToCart(event)
+		}
+
+		if (this.delete) {
+			this.deleteProductFromCart(event)
+		}
 	}
 
 	addProductToCart(event) {
@@ -129,26 +148,32 @@ class CartProducts {
 		this.render()
 	}
 
-	TODO:deleteProductFromCart(delButton) {
-		if (this.find.id === delButton.dataset['id'] && this.find.quantity > 1) {
+	deleteProductFromCart(event) {
+		if (this.find.quantity > 1) {
 			this.find.quantity--
 		} else {
-			userCart.splice(userCart.indexOf(this.find), 1);
-			document.querySelector(`.cart-item[data-id="${delButton.dataset['id']}"]`).remove();
+			userCart.splice(userCart.indexOf(this.find), 1)
+			document.querySelector(`.cart-item[data-id="${event.dataset['id']}"]`).remove()
 		}
 		this.render()
 	}
 
 	render() {
-		this.cartList = new Cart();
+		this.cartList = new Cart()
 		document.querySelector('.cart-block').innerHTML = this.cartList.render()
 	}
 }
 
+/**
+ * Класс отрисовывает корзину с товарами, считает подитог по каждому добавленному товару
+ * и нарастающий итог 
+ *
+ * @class Cart
+ */
 class Cart {
 	constructor() {
-		this.allProducts = '';
-		this.totalPrice = 0;
+		this.allProducts = ''
+		this.totalPrice = 0
 	}
 
 	render() {
@@ -159,11 +184,11 @@ class Cart {
                                 <div class="product-desc">
                                     <p class="product-title">${this.item.name}</p>
                                     <p class="product-quantity">Quantity: ${this.item.quantity}</p>
-                                    <p class="product-single-price">$${this.item.price} each</p>
+									<p class="product-single-price">$${this.item.price} each</p>
+									<p class="product-price">Subtotal: $ ${this.item.quantity * this.item.price}</p>
                                 </div>
                             </div>
                             <div class="right-block">
-                                <p class="product-price">${this.item.quantity * this.item.price}</p>
                                 <button class="del-btn" data-id="${this.item.id}">&times;</button>
                             </div>
 						</div>`
@@ -171,7 +196,7 @@ class Cart {
 		}
 
 		this.cartTotals = `<div class="cart-totals">
-							<div class="cart-total-sign">Your totals:</div>
+							<div class="cart-total-sign">Total:</div>
 							<div class="cart-total-price">$ ${this.totalPrice}</div>
 							</div>`;
 
